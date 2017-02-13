9c00b1c328a549ffd1ac144ccdbf6bc29bb61df0
Ellie Peterson <ekp2h92@gmail.com>
2017-02-13 17:03:25 -0500

    Added User.cpp and User.h to handle User objects for use in recalling created users later.

---
+++ User.h
@@ -0,0 +1,26 @@
+/*
+*   Author:   Ellie Peterson
+*   Project:  Autotester for wecounsel auto testing with Json API
+*   Purpose:  The goal of this application is to automate user testing on the WeCounsel application
+*
+*
+*
+*
+*/
+
+#ifndef __USER_H_INCLUDED__
+#define __USER_H_INCLUDED__
+
+#include <string>
+
+class User {
+  public: 
+    //@TODO
+    string getUserAsString() {};
+    
+    
+  private:
+    //@TODO
+}
+
+#endif
